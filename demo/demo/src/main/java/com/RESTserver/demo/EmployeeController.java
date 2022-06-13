ac0f6d2b516c5283ffd2aa7bc11d6bdf9b1ad879
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2022-06-13 05:46:49 -0700

    update

--- demo/demo/src/main/java/com/RESTserver/demo/EmployeeController.java
+++ demo/demo/src/main/java/com/RESTserver/demo/EmployeeController.java
@@ -4,7 +4,9 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +27,20 @@ class EmployeeController {
 
     // Get the list of employees
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    // List<Employee> all() {
+    //     return repository.findAll();
+    // }
+    //RESTFUL
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+        .map(employee -> EntityModel.of(employee,
+        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+        .collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+        linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+        
     }
 
     // Add a new employee
